d441ecde4071ea2cc9506af68b0c3cdc7c90fa32
Md Arin Alam <alamlew@gmail.com>
2024-03-03 21:40:28 +0530

    initialised the backend

---
+++ backend/src/index.ts
@@ -0,0 +1,46 @@
+import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  }
+}>()
+
+
+app.post('/api/v1/user/signup', async(c) => {
+  
+  const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+
+  return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/user/signin', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+
+app.get('/api/v1/blog/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+
+
+
+export default app
